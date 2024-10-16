176b9e992f59ff7080df7ce3c64f952688dd5676
Thành Đạt <honguyenthanhdat01012003@gmail.com>
2024-10-16 09:35:38 +0700

    up lai code

--- fictional-university-theme/single.php
+++
@@ -1,9 +0,0 @@
-<?php get_header(); ?>
-<?php
-while (have_posts()) {
-    the_post(); ?>
-    <h2><?php the_title(); ?></h2>
-    <?php the_content(); ?>
-    <hr>
-<?php } ?>
-<?php get_footer(); ?>
