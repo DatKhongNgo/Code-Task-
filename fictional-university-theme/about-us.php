6be367b45d0bda0d1f37a262db5b0511555caddf
Thành Đạt <honguyenthanhdat01012003@gmail.com>
2024-10-16 09:18:59 +0700

    push code Task

---
+++ fictional-university-theme/about-us.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(images/ocean.jpg)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title">Aubout Us</h1>
+        <div class="page-banner__intro">
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet velit debitis quas, sapiente voluptatibus accusamus a laborum ex animi minima ab dolorum placeat quidem provident quae. Maxime eveniet illum porro!</p>
+        </div>
+      </div>
+    </div>
+    <div class="page-section page-section--beige" style="background-color: white;">
+      <div class="container container--narrow generic-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia voluptates vero vel temporibus aliquid possimus, facere accusamus modi. Fugit saepe et autem, laboriosam earum reprehenderit illum odit nobis, consectetur dicta. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos molestiae, tempora alias atque vero officiis sit commodi ipsa vitae impedit odio repellendus doloremque quibusdam quo, ea veniam, ad quod sed.</p>
+      </div>
+    </div>
+<?php get_footer(); ?>
